66177ccaaf8e8d5c3355561a15cc649b269912b4
TmSalviano <tiagomsalviano@outlook.com>
2024-10-29 15:21:16 -0300

    test

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'I came to Rome as a city of bricks, left it a city of marble.';
+    return 'I came to Rome as a city of bricks, left it a city of marble';
   }
 }
