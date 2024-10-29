c34bfbec210211779ad9e64f4a2efce01d16470d
TmSalviano <tiagomsalviano@outlook.com>
2024-10-29 15:00:57 -0300

    first commit

---
+++ api/src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'I came to Rome as a city of bricks, left it a city of marble.';
+  }
+}
